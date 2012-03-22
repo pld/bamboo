933d78b097a9e72ac1acaf69e8cf3983c2aad36c
pld <peter@helioid.com>
2012-03-22 17:24:32 -0400

    csv to dict

--- root.py
+++ root.py
@@ -1,5 +1,9 @@
-import cherrypy
+import csv
+import json
 import urllib2
+
+import cherrypy
+
 from db import db
 
 class Root(object):
@@ -8,6 +12,19 @@ class Root(object):
         if doc:
             if 'http://' in doc or 'https://' in doc:
                 f = urllib2.urlopen(doc)
-                return f
+                d = self.csv_to_dict(f)
+                return json.dumps(d)
+                # create collection for file
+                # insert data into collection
         return "Hello World!"
     index.exposed = True
+
+
+    def csv_to_dict(self, f):
+        reader = csv.reader(f)
+        # assume first row is column headers
+        labels = reader.next()
+        data = []
+        return reduce(lambda ary, row:
+            ary + [reduce(lambda d, y: dict(d.items() + [(labels[y[0]], y[1])]),
+                    enumerate(row), {})], reader, [])
