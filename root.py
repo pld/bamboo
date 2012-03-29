5386cb52d2753f504ebfe7ec105939148ceec0bf
pld <peter@helioid.com>
2012-03-29 12:39:21 -0400

    use readcsv

--- root.py
+++ root.py
@@ -3,28 +3,20 @@ import json
 import urllib2
 
 import cherrypy
+from pandas import read_csv
 
 from db import db
 
 class Root(object):
 
-    def index(self, doc=None):
-        if doc:
-            if 'http://' in doc or 'https://' in doc:
-                f = urllib2.urlopen(doc)
-                d = self.csv_to_dict(f)
-                return json.dumps(d)
-                # create collection for file
-                # insert data into collection
-        return "Hello World!"
+    def index(self, d=None):
+        if d:
+            if 'http://' in d or 'https://' in d:
+                f = urllib2.urlopen(d)
+                df = read_csv(f, na_values=['n/a'])
+                r = df.to_dict()
+                return r
+                # TODO create collection for file
+                # TODO insert data into collection
+        return "Ohai World!"
     index.exposed = True
-
-
-    def csv_to_dict(self, f):
-        reader = csv.reader(f)
-        # assume first row is column headers
-        labels = reader.next()
-        data = []
-        return reduce(lambda ary, row:
-            ary + [reduce(lambda d, y: dict(d.items() + [(labels[y[0]], y[1])]),
-                    enumerate(row), {})], reader, [])
