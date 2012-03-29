3c105b303a8e3f6fb2f443b4486f8e2c51ee79d5
pld <peter@helioid.com>
2012-03-29 17:21:57 -0400

    store in mongo without munging ids

--- collection.py
+++ collection.py
@@ -1,12 +1,17 @@
 import json
+import urllib2
 
 import cherrypy
+from pandas import read_csv
 
 from db import db
+from utils import df_for_mongo
 
 SOURCE = '_source'
 
 class Collection(object):
+
+    collections = db().collections
 
     def __init__(self):
         pass
@@ -15,21 +20,21 @@ class Collection(object):
 
     def GET(self, d=None):
         if d:
-            r = [x for x in db()[d].find()]
+            r = [x for x in db().collections.find({SOURCE: d})]
             return json.dumps(r)
-        return "Ohai World!"
+        return 'Ohai World!'
 
     def POST(self, d=None):
         if d:
             if 'http://' in d or 'https://' in d:
                 f = urllib2.urlopen(d)
                 df = read_csv(f, na_values=['n/a'])
-                j = df_to_json(df)
+                j = df_for_mongo(df)
                 # add metadata to file
-                for i, e in enumerate(j):
+                for e in j:
                     e[SOURCE] = d
-                    j[i] = e
-                # store in db, create collection for file
-                # insert data into collection
-                db()[d].insert(j)
-                return json.dumps(j)
+                # insert data into collections
+                object_ids = db().collections.insert(j)
+                return json.dumps({
+                    'object_ids': [str(_id) for _id in object_ids]
+                })
--- utils.py
+++ utils.py
@@ -1,8 +1,33 @@
+from math import isnan
+
+
+MONGO_RESERVED_KEYS = ['_id']
+MONGO_RESERVED_KEY_PREFIX = 'MONGO_RESERVED_KEY_'
+
+
+def is_float_nan(n):
+    return isinstance(n, float) and isnan(n)
+
+
+def df_for_mongo(df):
+    df = df_to_json(df)
+    for e in df:
+        for k, v in e.items():
+            if k in MONGO_RESERVED_KEYS:
+                e[encode_key_for_mongo(k)] = v
+                del e[k]
+    return df
+
+
+def encode_key_for_mongo(k):
+    return '%s%s' % (MONGO_RESERVED_KEY_PREFIX, k)
+
+
 def df_to_json(df):
     return [
         dict([
-            (colname, row[i])
-            for i,colname in enumerate(df.columns)
+            (colname, 'null') if is_float_nan(row[i]) else (colname, row[i])
+            for i, colname in enumerate(df.columns)
         ])
         for row in df.values
     ]
