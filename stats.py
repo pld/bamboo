1a481be9d94ed1efeff1576739130d71f69bc3b8
pld <peter@helioid.com>
2012-04-12 13:35:23 -0400

    do not remove 1 and max

--- stats.py
+++ stats.py
@@ -8,9 +8,8 @@ def parse_value_counts(value_counts):
 
 def summary_stats(dtype, data):
     value_counts = parse_value_counts(data.value_counts())
-    if len(value_counts) > 1 and value_counts.max() > 1:
-        return {
-            np.object_: value_counts,
-            np.float64: data.describe(),
-        }.get(dtype.type, None)
+    return {
+        np.object_: value_counts,
+        np.float64: data.describe(),
+    }.get(dtype.type, None)
     return None
