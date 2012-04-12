9b79e97849d8874a4fa5f495e8047564ff5b8166
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-04-12 13:15:53 -0400

    enabling access to a static directory.

--- bamboo.py
+++ bamboo.py
@@ -3,18 +3,8 @@ from calculate import Calculate
 from collection import Collection
 from root import Root
 
-conf = {
-    'global': {
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': 8080,
-    },
-    '/': {
-        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-    },
-}
-
 root = Root()
 root.calculate = Calculate()
 root.collection = Collection()
 
-cherrypy.quickstart(root, '/', conf)
+cherrypy.quickstart(root, config='prod.conf') 
