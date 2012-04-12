f557c4499e4b3e0a9679bef9c26583393a9910cf
pld <peter@helioid.com>
2012-04-12 13:04:28 -0400

    label stat colums, filter out nulls

--- stats.py
+++ stats.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+def parse_value_counts(value_counts):
+    if value_counts.get('null'):
+        return value_counts.drop(['null'])
+    return value_counts
+
 
 def summary_stats(dtype, data):
-    value_counts = data.value_counts()
+    value_counts = parse_value_counts(data.value_counts())
     if len(value_counts) > 1 and value_counts.max() > 1:
         return {
             np.object_: value_counts,
