2131944112d0c3fb69285228e587f7820e1677b9
pld <peter@helioid.com>
2012-04-17 14:52:20 -0400

    better abstraction for parse value

--- stats.py
+++ stats.py
@@ -1,16 +1,16 @@
 import numpy as np
 from pandas import pivot_table
 
-def parse_value_counts(value_counts):
+def parse_value_counts(data):
+    value_counts = data.value_counts()
     if value_counts.get('null'):
         return value_counts.drop(['null'])
     return value_counts
 
 
 def summary_stats(dtype, data):
-    value_counts = parse_value_counts(data.value_counts())
     return {
-        np.object_: value_counts,
+        np.object_: parse_value_counts(data),
         np.float64: data.describe(),
     }.get(dtype.type, None)
     return None
