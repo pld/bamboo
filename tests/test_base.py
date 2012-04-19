72884cdc340aebbb0b15e6e61bbe41c36d3edd91
Mark Johnston <mejymejy@gmail.com>
2012-04-19 15:56:44 -0400

    trying to figure out cherrypy testing framework

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,13 +1,37 @@
 import unittest
 
+import cherrypy
+from cherrypy.test.helper import CPWebCase
+
 from decorators import requires_internet
 
 
-class TestBase(unittest.TestCase):
+class TestBase(CPWebCase):
 
     def setUp(self):
         self._load_test_data()
+        #self._start_cherrypy()
+        #super(TestBase, self).setUp()
+
+    def tearDown(self):
+        pass
+        #self._stop_cherrypy()
+        #super(TestBase, self).tearDown()
+
+    def _start_cherrypy(self):
+        cherrypy.config.update({
+            'server.logToScreen': False,
+            'log.screen': False,
+            'environment': 'embedded',
+        })
+        cherrypy.engine.start()
+
+    def _stop_cherrypy(self):
+        cherrypy.engine.stop()
 
     @requires_internet
     def _load_test_data(self):
         pass
+
+    def test_true(self):
+        self.assertTrue(True)
