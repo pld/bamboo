8e79b02c4189dabcf80b0e0852b4979a378e5404
pld <peter@helioid.com>
2012-05-04 15:30:07 -0400

    remove deprecated files from bamboo.py

--- bamboo.py
+++ bamboo.py
@@ -1,11 +1,9 @@
 import cherrypy
 
-from controllers.calculate import Calculate
 from controllers.datasets import Datasets
 from controllers.root import Root
 
 root = Root()
-root.calculate = Calculate()
 root.datasets = Datasets()
 
 cherrypy.quickstart(root, config='config/prod.conf')
