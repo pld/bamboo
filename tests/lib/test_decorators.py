2af55fc2965fa7f735a03ba3684d96cce0346949
pld <peter@helioid.com>
2012-05-23 17:25:05 -0400

    decorators test file

---
+++ tests/lib/test_decorators.py
@@ -0,0 +1,22 @@
+#import lib.decorators
+from lib import decorators
+from tests.test_base import TestBase
+
+class TestDecorators(TestBase):
+
+    def _test_decorator(self, func):
+        def test_func():
+            pass
+        wrapped_test_func = func(test_func)
+        self.assertTrue(hasattr(wrapped_test_func, '__call__'))
+        wrapped_test_func()
+
+    def test_print_time(self):
+        self._test_decorator(decorators.print_time)
+
+    def test_requires_internet(self):
+        self._test_decorator(decorators.requires_internet)
+
+    def test_check_bad_url(self):
+        result = decorators._check_url('http://')
+        self.assertEqual(result, False)
