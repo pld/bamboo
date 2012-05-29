d86cc20c8b23381be1ee4dcb5aed4db6d7ea7c1b
modilabs-dev <modilabs@googlegroups.com>
2012-05-29 21:16:49 +0000

    server control file

---
+++ bin/bamboo.sh
@@ -0,0 +1,81 @@
+#! /bin/sh
+
+# PATH should only include /usr/* if it runs after the mountnfs.sh script
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+DESC="bamboo server"
+NAME=bamboo
+PIDFILE=/var/www/bamboo/shared/pids/$NAME.pid
+SCRIPTNAME=/var/www/bamboo/current/bin/$NAME.sh
+DAEMON=/home/bamboo/.virtualenvs/bamboo/bin/cherryd
+DAEMON_ARGS="-i bamboo -c /var/www/bamboo/current/config/prod.conf -p $PIDFILE -d"
+
+# load the virtualenv
+. /home/bamboo/.virtualenvs/bamboo/bin/activate
+
+# Exit if the package is not installed
+[ -x "$DAEMON" ] || exit 0
+
+#
+# Function that starts the daemon/service
+#
+do_start()
+{
+    $DAEMON $DAEMON_ARGS
+}
+
+#
+# Function that stops the daemon/service
+#
+do_stop()
+{
+    kill `cat $PIDFILE`
+}
+
+case "$1" in
+  start)
+	[ "$VERBOSE" != no ] && echo "Starting $DESC" "$NAME"
+	do_start
+	case "$?" in
+		0|1) [ "$VERBOSE" != no ] && echo 0 ;;
+		2) [ "$VERBOSE" != no ] && echo 1 ;;
+	esac
+	;;
+  stop)
+	[ "$VERBOSE" != no ] && echo "Stopping $DESC" "$NAME"
+	do_stop
+	case "$?" in
+		0|1) [ "$VERBOSE" != no ] && echo 0 ;;
+		2) [ "$VERBOSE" != no ] && echo 1 ;;
+	esac
+	;;
+  status)
+       status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
+       ;;
+  restart|force-reload)
+	#
+	# If the "reload" option is implemented then remove the
+	# 'force-reload' alias
+	#
+	echo "Restarting $DESC" "$NAME"
+	do_stop
+	case "$?" in
+	  0|1)
+		do_start
+		case "$?" in
+			0) echo 0 ;;
+			1) echo 1 ;; # Old process is still running
+			*) echo 1 ;; # Failed to start
+		esac
+		;;
+	  *)
+	  	# Failed to stop
+		echo 1
+		;;
+	esac
+	;;
+  *)
+	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
+	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
+	exit 3
+	;;
+esac
