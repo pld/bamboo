0442e7f3e2930342ef372dc764c01686dee46a52
modilabs-dev <modilabs@googlegroups.com>
2012-06-14 17:37:47 -0400

    expose version controller

--- controllers/version.py
+++ controllers/version.py
@@ -5,6 +5,8 @@ from lib.constants import VERSION_DESCRIPTION, VERSION_NUMBER
 
 class Version(object):
 
+    exposed = True
+
     def GET(self):
         return json.dumps({'version': VERSION_NUMBER, 'description':
                 VERSION_DESCRIPTION})
