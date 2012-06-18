30830ef8cb5d6fd52dd5333f61def0e68fad2d96
modilabs-dev <modilabs@googlegroups.com>
2012-06-18 15:58:06 -0400

    update commands, better comments, fixing host select

--- bin/commands.sh
+++ bin/commands.sh
@@ -1,10 +1,11 @@
 # var for bamboo server
 
+HOST="http://bamboo.io"
+
 while getopts l opt
 do
   case "$opt" in
     l) HOST='http://localhost:8080' ;;
-    *) HOST='http://bamboo.io' ;;
   esac
 done
 
@@ -21,14 +22,14 @@ ID=`echo "$RET" | sed 's/.*: "\(\w*\).*/\1/'`
 echo "The dataset id is $ID"
 
 echo -e "\nRetrieve info for dataset"
-RET=$(curl -#g $HOST/datasets/$ID?info=t)
+RET=$(curl -#g $HOST/datasets/$ID/info)
 echo $RET
 
-echo -e "\nRetrieve data"
+echo -e "\nRetrieve data for School Zone RWIKA"
 RET=$(curl -#g $HOST/datasets/$ID?query='{"School%20Zone":"RWIKA"}')
 echo $RET
 
-echo -e "\nCalculate summary statistics"
+echo -e "\nCalculate summary statistics for School Zone RWIKA"
 RET=$(curl -#g $HOST/datasets/$ID/summary?query='{"School%20Zone":"RWIKA"}')
 echo $RET
 
@@ -36,10 +37,10 @@ echo -e "\nCalculate summary statistics with a grouping (truncated)"
 RET=$(curl -#g $HOST/datasets/$ID/summary?query='{"Public%20or%20Private":"PRIVATE"}'\&group=District)
 echo $RET | cut -c -1000
 
-echo -e "\nStore calculation formula"
+echo -e "\nStore calculation named small_schools with formula Acreage<10"
 RET=$(curl -#X POST -d "name=small_schools&formula=Acreage<10" $HOST/calculations/$ID)
 echo $RET
 
-echo -e "\nRetrieve new calculated column (truncated)"
-RET=$(curl -#g $HOST/datasets/$ID?select='{"small_schools":1}')
+echo -e "\nRetrieve new calculated column small_schools and Districts (truncated)"
+RET=$(curl -#g $HOST/datasets/$ID?select='{"small_schools":1,"District":1}')
 echo $RET | cut -c -1000
