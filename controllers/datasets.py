38ffb01c68daad9f431f9af4f707cf61477bc921
modilabs-starscream <modilabs.starscream@gmail.com>
2012-08-28 09:53:07 -0400

    comment on else

--- controllers/datasets.py
+++ controllers/datasets.py
@@ -96,6 +96,7 @@ class Datasets(object):
                     if potential_name not in existing_dframe.columns:
                         new_column.name = labels_to_slugs[potential_name]
                     else:
+                        # TODO is this block unreachable?
                         new_column.name = potential_name
                     new_dframe = new_dframe.join(new_column)
                 # merge the two
