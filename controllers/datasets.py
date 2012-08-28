126760966b3e2880308b36adea1e79c54c510d1c
modilabs-starscream <modilabs.starscream@gmail.com>
2012-08-28 11:34:54 -0400

    update todo

--- controllers/datasets.py
+++ controllers/datasets.py
@@ -96,7 +96,7 @@ class Datasets(object):
                     if potential_name not in existing_dframe.columns:
                         new_column.name = labels_to_slugs[potential_name]
                     else:
-                        # TODO is this block unreachable?
+                        # TODO this block is uncovered
                         new_column.name = potential_name
                     new_dframe = new_dframe.join(new_column)
                 # merge the two
