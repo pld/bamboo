3ddad3e1f59dc500b3cd54d5cfc6c2ea6301f950
renaud gaudin <rgaudin@gmail.com>
2012-09-05 12:10:25 +0000

    fixed uwsgi params

--- scripts/bamboo_uwsgi.sh
+++ scripts/bamboo_uwsgi.sh
@@ -2,12 +2,13 @@
 
 UWSGI_BIN=/home/bamboo/.virtualenvs/bamboo/bin/uwsgi
 WSGI_APP=/var/www/bamboo/current/bamboo/bambooapp.py
-SOCKET=localhost:8099
-PROCESSES=8
+UWSGI_SOCKET=localhost:8099
+UWSGI_PROCESSES=8
 ROOT_DIR=/var/www/bamboo/current/bamboo
-LOG_FILE=/var/www/bamboo/log/uwsgi_bamboo.log
-UID=bamboo
-GID=bamboo
+UWSGI_LOG_FILE=/var/www/bamboo/log/uwsgi_bamboo.log
+UWSGI_PID_FILE=/var/www/bamboo/shared/pids/uwsgi_bamboo.pid
+UWSGI_UID=bamboo
+UWSGI_GID=bamboo
 VENV=/home/bamboo/.virtualenvs/bamboo
 
-${UWSGI_BIN} --touch-reload ${WSGI_APP} --socket ${SOCKET} --protocol uwsgi --processes ${PROCESSES} --enable-threads --chdir ${ROOT_DIR} --module bambooapp:application --virtualenv ${VENV} --master --pythonpath ${ROOT_DIR} --pythonpath ${ROOT_DIR}/../ --daemonize ${LOG_FILE} --uid ${UID} --gid ${GID}
+${UWSGI_BIN} --touch-reload ${WSGI_APP} --socket ${UWSGI_SOCKET} --protocol uwsgi --processes ${UWSGI_PROCESSES} --enable-threads --chdir ${ROOT_DIR} --module bambooapp:application --virtualenv ${VENV} --master --pythonpath ${ROOT_DIR} --pythonpath ${ROOT_DIR}/../ --daemonize ${UWSGI_LOG_FILE} --uid ${UWSGI_UID} --gid ${UWSGI_GID} --vacuum --pidfile2 ${UWSGI_PID_FILE}
