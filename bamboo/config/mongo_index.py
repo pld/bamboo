b5432bb4e60189ab3a93d500fe051fd3796920f9
pld <peter@helioid.com>
2012-09-07 15:51:09 -0400

    pep8ify

--- bamboo/config/mongo_index.py
+++ bamboo/config/mongo_index.py
@@ -2,16 +2,21 @@
 This Script is to ensure the three collections of bamboo being indexed.
 """
 from pymongo import Connection, ASCENDING
+
+
 def ensure_indexing():
-    connection = Connection('localhost',27017)
+    connection = Connection('localhost', 27017)
     db = connection.bamboo_dev
     calculations = db.calculations
     datasets = db.datasets
     observations = db.observations
-    datasets.ensure_index([("BAMBOO_RESERVED_KEY_dataset_id",ASCENDING),
-            ("BAMBOO_RESERVED_KEY_dataset_observation_id",ASCENDING)])
-    observations.ensure_index([("BAMBOO_RESERVED_KEY_dataset_observation_id",ASCENDING)])
-    calculations.ensure_index([("BAMBOO_RESERVED_KEY_dataset_id",ASCENDING)])
+    datasets.ensure_index([
+        ("BAMBOO_RESERVED_KEY_dataset_id", ASCENDING),
+        ("BAMBOO_RESERVED_KEY_dataset_observation_id", ASCENDING)])
+    observations.ensure_index([
+        ("BAMBOO_RESERVED_KEY_dataset_observation_id", ASCENDING)])
+    calculations.ensure_index([("BAMBOO_RESERVED_KEY_dataset_id", ASCENDING)])
+
 
 if __name__ == "__main__":
     ensure_indexing()
