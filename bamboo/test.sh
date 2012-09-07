d73d0eb42fb07190f91347b4e14d9e86c19b9724
pld <peter@helioid.com>
2012-09-07 15:54:42 -0400

    for profile

--- bamboo/test.sh
+++ bamboo/test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 nosetests --with-progressive --with-cov --cov-report term-missing ../bamboo
+
+# for profiling
+# nosetests --with-profile tests/models/test_dataset.py --profile-restrict 'models/dataset'
