10442d550f1a98e2d5c0bd43d53dc835e2606677
pld <peter@helioid.com>
2012-10-10 00:09:14 -0400

    move test decorators, closes #187

--- bamboo/lib/decorators.py
+++ bamboo/lib/decorators.py
@@ -1,49 +1,4 @@
-from functools import wraps
-import time
-import urllib2
-
-
 class classproperty(property):
 
     def __get__(self, cls, owner):
         return self.fget.__get__(None, owner)()
-
-
-def print_time(func):
-    """
-    @print_time
-
-    Put this decorator around a function to see how many seconds each
-    call of this function takes to run.
-    """
-    def wrapped_func(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        seconds = end - start
-        print "SECONDS:", seconds, func.__name__, kwargs
-        return result
-    return wrapped_func
-
-
-def requires_internet(f, url='http://74.125.113.99'):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if not _internet_on(url):
-            raise AssertionError('This test requires an internet connection to'
-                                 'run/pass.')
-        return f(*args, **kwargs)
-    return wrapper
-
-
-def _check_url(url, timeout=1):
-    try:
-        response = urllib2.urlopen(url, timeout=timeout)
-        return True
-    except urllib2.URLError as e:
-        pass
-    return False
-
-
-def _internet_on(url):
-    return _check_url(url)
--- bamboo/tests/lib/test_decorators.py
+++ bamboo/tests/lib/test_decorators.py
@@ -1,4 +1,5 @@
 from bamboo.lib import decorators
+from bamboo.tests imprt decorators as test_decorators
 from bamboo.tests.test_base import TestBase
 
 
@@ -15,17 +16,17 @@ class TestDecorators(TestBase):
         wrapped_test_func()
 
     def test_print_time(self):
-        self._test_decorator(decorators.print_time)
+        self._test_decorator(test_decorators.print_time)
 
     def test_requires_internet(self):
-        self._test_decorator(decorators.requires_internet)
+        self._test_decorator(test_decorators.requires_internet)
 
     def test_requires_internet_bad_url(self):
-        wrapped_test_func = decorators.requires_internet(self._test_func,
-                                                         'http://')
+        wrapped_test_func = test_decorators.requires_internet(
+            self._test_func, 'http://')
         self.assertTrue(hasattr(wrapped_test_func, '__call__'))
         self.assertRaises(AssertionError, wrapped_test_func)
 
     def test_check_bad_url(self):
-        result = decorators._check_url('http://')
+        result = test_decorators._check_url('http://')
         self.assertEqual(result, False)
