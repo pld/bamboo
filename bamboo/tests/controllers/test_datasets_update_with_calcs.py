08b2d93c5f40db09cc6c4466bb67d7bef1141b05
modilabs-starscream <modilabs.starscream@gmail.com>
2012-10-10 23:02:11 -0400

    typo

--- bamboo/tests/controllers/test_datasets_update_with_calcs.py
+++ bamboo/tests/controllers/test_datasets_update_with_calcs.py
@@ -53,7 +53,7 @@ class TestDatasetsUpdateWithCalcs(TestAbstractDatasets):
         result = json.loads(self.controller.POST(
             merge=True,
             datasets=json.dumps([self.dataset1_id, self.dataset2_id])))
-        self.merged_dataset1_id = result[Datset.ID]
+        self.merged_dataset1_id = result[Dataset.ID]
 
         result = json.loads(self.controller.POST(
             merge=True,
