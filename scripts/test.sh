1ab3a10ae20cacba56870a8af7c293a78916ddc3
modilabs-starscream <modilabs.starscream@gmail.com>
2012-10-15 14:44:53 -0400

    example profile call

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,5 +15,6 @@ if [ -z "$PROFILE" ]
 then
   nosetests --with-progressive --with-cov --cov-report term-missing ../bamboo
 else
+  # E.g. FILE=tests/models/test_dataset.py PROFILE=models/dataset
   nosetests --with-profile $FILE --profile-restrict $PROFILE
 fi
