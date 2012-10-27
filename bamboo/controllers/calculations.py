cef6eaaf09233e8fe3b04d208cecace06cf7ae3b
pld <peter@helioid.com>
2012-10-27 14:47:48 -0400

    comments and callback cleanup

--- bamboo/controllers/calculations.py
+++ bamboo/controllers/calculations.py
@@ -8,28 +8,34 @@ from bamboo.models.dataset import Dataset
 
 
 class Calculations(AbstractController):
+    """The Calculations Controller provides access to calculations.
+
+    Calculations are formulas and names that (for now) must be linked to a
+    specific dataset via that dataset's ID. All actions in the Calculations
+    Controller can optionally take a *callback* parameter.  If passed the
+    returned result will be wrapped this the parameter value.  E.g., is
+    ``callback=parseResults`` the returned value will be
+    ``parseResults([some-JSON])``, where ``some-JSON`` is the function return
+    value.
     """
-    The Calculations Controller provides access to calculations.  Calculations
-    are formulas and names
-    that (for now) must be linked to a specific dataset via that dataset's ID.
 
-    All actions in the Calculations Controller can optionally take a *callback*
-    parameter.  If passed the returned result will be wrapped this the
-    parameter value.  E.g., is ``callback=parseResults`` the returned value
-    will be ``parseResults([some-JSON])``, where ``some-JSON`` is the function
-    return value.
-    """
+    def delete(self, dataset_id, name, group=None):
+        """Delete the calculation with *name* from the dataset.
 
-    def delete(self, dataset_id, name, group=None, callback=False):
-        """
-        Delete the calculation for the dataset specified by the hash
-        *dataset_id* from mongo and the column *name*.
+        Delete the calculation with column *name* from the dataset specified by
+        the hash *dataset_id* from mongo. If it is an aggregate calculation a
+        *group* must also be passed to determine the correct aggregate
+        calculation to delete. This will also remove the column *name* from the
+        dataframe for the dataset or the aggregate dataset.
 
-        If it is an aggregate calculation a *group* must also be passed to
-        determine the correct aggregate calculation to delete.
+        Args:
+            dataset_id: The dataset ID for which to delete the calculation.
+            name: The name of the calculation to delete.
+            group: The group of the calculation to delete, if an aggregation.
 
-        This will also remove the column *name* from the data frame for
-        dataset.
+        Returns:
+            JSON with success if delete or an error string if the calculation
+            could not be found.
         """
         result = None
 
@@ -40,31 +46,46 @@ class Calculations(AbstractController):
             result = {self.SUCCESS: 'deleted calculation: %s for dataset: %s' %
                       (name, dataset_id)}
         return self.dump_or_error(result,
-                                  'name and dataset_id combination not found',
-                                  callback)
+                                  'name and dataset_id combination not found')
 
-    def create(self, dataset_id, formula, name, group=None, callback=False):
-        """
+    def create(self, dataset_id, formula, name, group=None):
+        """Add a calculation to a dataset with the given fomula, etc.
+
         Create a new calculation for *dataset_id* named *name* that calulates
         the *formula*.  Variables in formula can only refer to columns in the
         dataset.
+
+        Args:
+            dataset_id: The dataset ID to add the calculation to.
+            formula: The formula for the calculation which must match the
+                parser language.
+            name: The name to assign the new column for this formula.
+            group: A column to group by for aggregations, must be a dimension.
+
+        Returns:
+            A success string is the calculation is create. An error string if
+            the dataset could not be found, the formula could not be parsed, or
+            the group was invalid.
         """
-        error = 'dataset_id not found'
-        result = None
-        dataset = Dataset.find_one(dataset_id)
-        if dataset:
-            try:
-                Calculation.create(dataset, formula, name, group)
-                result = {
-                    self.SUCCESS: 'created calulcation: %s for dataset: %s'
-                    % (name, dataset_id)}
-            except ParseError as e:
-                error = e.__str__()
-        return self.dump_or_error(result, error, callback)
+        def _action(dataset, formula=formula, name=name, group=group):
+            Calculation.create(dataset, formula, name, group)
+            return {
+                self.SUCCESS: 'created calulcation: %s for dataset: %s'
+                % (name, dataset_id)}
+        return self._safe_get_and_call(
+            dataset_id, _action, formula=formula, name=name, group=group,
+            exceptions=(ParseError,))
 
     def show(self, dataset_id, callback=False):
-        """
-        Retrieve the calculations for *dataset_id*.
+        """Retrieve the calculations for *dataset_id*.
+
+        Args:
+            dataset_id: The dataset to show calculations for.
+            callback: A JSONP callback function string.
+
+        Returns:
+            A list of calculation records.  Each calculation record shows the
+            calculations name, formula, group (if it exists), and state.
         """
         dataset = Dataset.find_one(dataset_id)
         result = None
--- bamboo/controllers/datasets.py
+++ bamboo/controllers/datasets.py
@@ -5,7 +5,6 @@ from bamboo.controllers.abstract_controller import AbstractController,\
     ArgumentError
 from bamboo.core.merge import merge_dataset_ids, MergeError
 from bamboo.core.summary import ColumnTypeError
-from bamboo.lib.jsontools import JSONError
 from bamboo.lib.io import create_dataset_from_url, create_dataset_from_csv
 from bamboo.models.calculation import Calculation
 from bamboo.models.dataset import Dataset
@@ -22,12 +21,24 @@ class Datasets(AbstractController):
     parameter value.  E.g., is ``callback=parseResults`` the returned value
     will be ``parseResults([some-JSON])``, where ``some-JSON`` is the function
     return value.
+
+    Attributes:
+        SELECT_ALL_FOR_SUMMARY: a string the the client can pass to
+        indicate that all columns should be summarized.
     """
     SELECT_ALL_FOR_SUMMARY = 'all'
 
-    def delete(self, dataset_id, callback=False):
-        """
-        Delete the dataset with hash *dataset_id* from mongo
+    def delete(self, dataset_id):
+        """Delete the dataset with hash *dataset_id*.
+
+        This also deletes any observations associated with the dataset_id. The
+        dataset and observations are not recoverable.
+
+        Args:
+            dataset_id: The dataset ID of the dataset to be deleted.
+
+        Returns:
+            A string of success or error if that dataset could not be found.
         """
         dataset = Dataset.find_one(dataset_id)
         result = None
@@ -35,28 +46,47 @@ class Datasets(AbstractController):
         if dataset.record:
             dataset.delete()
             result = {self.SUCCESS: 'deleted dataset: %s' % dataset_id}
-        return self.dump_or_error(result, 'id not found', callback)
+        return self.dump_or_error(result, 'id not found')
 
     def info(self, dataset_id, callback=False):
-        """
-        Return the data for *dataset_id*. Returns an error message if
-        *dataset_id* does not exist.
+        """Fetch and return the meta-data for a dataset.
+
+        Args:
+            dataset_id: The dataset ID of the dataset to return meta-data for.
+            callback: A JSONP callback function to wrap the result in.
+
+        Returns:
+            The data for *dataset_id*. Returns an error message if
+            *dataset_id* does not exist.
         """
         def _action(dataset):
             return dataset.info()
-        return self._safe_get_and_call(dataset_id, _action, callback)
+        return self._safe_get_and_call(dataset_id, _action, callback=callback)
 
     def summary(self, dataset_id, query=None, select=None,
                 group=None, limit=0, order_by=None, callback=False):
-        """
+        """Return a summary of the dataset ID given the passed parameters.
 
-          - The *select* argument is required, it can be 'all' or a MongoDB
-            JSON query
-          - If *group* is passed group the summary.
-          - If *query* is passed restrict summary to rows matching query.
+        Retrieve the dataset by ID then limit that data using the optional
+        *query*, *select* and *limit* parameters. Summarize the resulting data
+        potentially grouping by the optional *group* parameter. Order the
+        results using *order_by* if passed.
 
-        Returns an error message if *dataset_id* does not exist or the JSON for
-        query or select is improperly formatted.
+        Args:
+            dataset_id: The dataset ID of the dataset to summarize.
+            select: This is a required argument, it can be 'all' or a MongoDB
+                JSON query
+            group: If passed, group the summary by this column or list of
+                columns.
+            query: If passed restrict summary to rows matching this query.
+            limit: If passed limit the rows to summarize to this number.
+            order_by: If passed order the result using this column.
+            callback: A JSONP callback function to wrap the result in.
+
+        Returns:
+            An error message if *dataset_id* does not exist or the JSON for
+            query or select is improperly formatted.  Otherwise the summary as
+            a JSON string.
         """
         def _action(dataset, query=query, select=select, group=group,
                     limit=limit, order_by=order_by):
@@ -67,27 +97,49 @@ class Datasets(AbstractController):
             return dataset.summarize(dataset, query, select,
                                      group, limit=limit,
                                      order_by=order_by)
-        return self._safe_get_and_call(dataset_id, _action, callback)
+        return self._safe_get_and_call(dataset_id, _action, callback=callback,
+                                       exceptions=(ColumnTypeError,))
 
     def related(self, dataset_id, callback=False):
-        """
-        Return a dict of aggregated data for the given *dataset_id*.
-        The dict is of the form {group: id}.
+        """Return a dict of aggregated data for the given *dataset_id*.
 
-        Returns an error message if *dataset_id* does not exist.
+        Args:
+            dataset_id: The dataset ID of the dataset to return related data
+                for.
+            callback: A JSONP callback function to wrap the result in.
+
+        Returns:
+            An error message if *dataset_id* does not exist. Otherwise, returns
+            a dict of the form {[group]: [id]}.
         """
         def _action(dataset):
             return dataset.aggregated_datasets_dict
-        return self._safe_get_and_call(dataset_id, _action, callback)
+        return self._safe_get_and_call(dataset_id, _action, callback=callback)
 
     def show(self, dataset_id, query=None, select=None,
              group=None, limit=0, order_by=None, callback=False):
-        """
-        Return rows for *dataset_id*, passing *query* and *select* onto
-        MongoDB.
+        """ Return rows for *dataset_id*, matching the passed parameters.
 
-        Returns an error message if *dataset_id* does not exist or the JSON for
-        query or select is improperly formatted.
+        Retrieve the dataset by ID then limit that data using the optional
+        *query*, *select* and *limit* parameters. Order the results using
+        *order_by* if passed.  The return the results grouped by the *group*
+        parameter if it is passed.
+
+        Args:
+            dataset_id: The dataset ID of the dataset to return.
+            select: This is a required argument, it can be 'all' or a MongoDB
+                JSON query
+            group: If passed, group the resuts by this column or list of
+                columns.
+            query: If passed restrict results to rows matching this query.
+            limit: If passed limit the rows to this number.
+            order_by: If passed order the result using this column.
+            callback: A JSONP callback function to wrap the result in.
+
+        Returns:
+            An error message if *dataset_id* does not exist or the JSON for
+            query or select is improperly formatted. Otherwise a JSON string of
+            the rows matching the parameters.
         """
         def _action(dataset, query=query, select=select,
                     limit=limit, order_by=order_by):
@@ -95,17 +147,19 @@ class Datasets(AbstractController):
                 query=query, select=select,
                 limit=limit, order_by=order_by).to_jsondict()
 
-        return self._safe_get_and_call(dataset_id, _action, callback)
+        return self._safe_get_and_call(dataset_id, _action, callback=callback)
 
-    def merge(self, datasets=None, callback=False):
-        """
-        Merge the datasets with the dataset_ids in *datasets*.
+    def merge(self, datasets=None):
+        """Merge the datasets with the dataset_ids in *datasets*.
 
-        *dataset* should be a JSON encoded array of dataset IDs for existing
-        datasets.
+        Args:
+            dataset: A JSON encoded array of dataset IDs for existing
+                datasets.
 
-        Returns the ID of the new merged datasets created by combining the
-        datasets provided as an argument.
+        Returns:
+            An error if the datasets could not be found or less than two
+            dataset IDs were passed.  Otherwise, the ID of the new merged
+            dataset created by combining the datasets provided as an argument.
         """
         result = None
         error = 'merge failed'
@@ -116,46 +170,61 @@ class Datasets(AbstractController):
         except (ValueError, MergeError) as e:
             error = e.__str__()
 
-        return self.dump_or_error(result, error, callback)
+        return self.dump_or_error(result, error)
 
-    def create(self, url=None, csv_file=None, callback=False):
-        """
-        If *url* is provided read data from URL *url*.
-        If *csv_file* is provided read data from *csv_file*.
-        If neither are provided return an error message.  Also return an error
-        message if an improperly formatted value raises a ValueError, e.g. an
-        improperly formatted CSV file.
+    def create(self, url=None, csv_file=None):
+        """Create a dataset by URL or passed CSV file.
+
+        If *url* is provided, create a dataset by downloading a CSV from that
+        URL. If *url* is not provided and *csv_file* is provided, create a
+        dataset with the data in the passed *csv_file*. If both *url* and
+        *csv_file* are provided, *csv_file* is ignored.
 
         The follow words are reserved and will lead to unexpected behavior if
         used as column names:
 
-            - sum
-            - date
-            - years
+            - all
             - and
-            - or
-            - not
-            - in
+            - case
+            - date
             - default
+            - in
+            - not
+            - or
+            - sum
+            - years
 
+        Args:
+            url: A URL to load a CSV file from. The URL must point to a CSV
+                file.
+            csv_file: An uploaded CSV file to read from.
+
+        Returns:
+            An error message ff neither *url* nor *csv_file* are provided. An
+            error message if an improperly formatted value raises a ValueError,
+            e.g. an improperly formatted CSV file. An error message if the URL
+            could not be loaded. Otherwise returns a JSON string with the
+            dataset ID of the newly created dataset.  Note that the dataset
+            will not be fully loaded until its state is set to ready.
         """
         result = None
         error = 'url or csv_file required'
 
-        try:
-            if url:
-                dataset = create_dataset_from_url(url)
-            elif csv_file:
-                dataset = create_dataset_from_csv(csv_file)
-            result = {Dataset.ID: dataset.dataset_id}
-        except IOError:
-            error = 'could not get a filehandle for: %s' % csv_file
-        except urllib2.URLError:
-            error = 'could not load: %s' % url
+        if url or csv_file:
+            try:
+                if url:
+                    dataset = create_dataset_from_url(url)
+                elif csv_file:
+                    dataset = create_dataset_from_csv(csv_file)
+                result = {Dataset.ID: dataset.dataset_id}
+            except IOError:
+                error = 'could not get a filehandle for: %s' % csv_file
+            except urllib2.URLError:
+                error = 'could not load: %s' % url
 
-        return self.dump_or_error(result, error, callback)
+        return self.dump_or_error(result, error)
 
-    def update(self, dataset_id, callback=False):
+    def update(self, dataset_id):
         """
         Update the *dataset_id* with the body as JSON.
         """
@@ -165,9 +234,9 @@ class Datasets(AbstractController):
         if dataset.record:
             dataset.add_observations(cherrypy.request.body.read())
             result = {Dataset.ID: dataset_id}
-        return self.dump_or_error(result, error, callback)
+        return self.dump_or_error(result, error)
 
-    def drop_columns(self, dataset_id, columns, callback=False):
+    def drop_columns(self, dataset_id, columns):
         """
         Drop columns in dataset with id *dataset_id*.  *columns* is an array of
         columns within the dataset.  Returns an error if any column is not in
@@ -177,17 +246,4 @@ class Datasets(AbstractController):
             dataset.drop_columns(columns)
             return {self.SUCCESS: 'in dataset %s dropped columns: %s' %
                     (dataset_id, columns)}
-        return self._safe_get_and_call(dataset_id, _action, callback)
-
-    def _safe_get_and_call(self, dataset_id, action, callback, **kwargs):
-        dataset = Dataset.find_one(dataset_id)
-        error = 'id not found'
-        result = None
-
-        try:
-            if dataset.record:
-                result = action(dataset, **kwargs)
-        except (ArgumentError, ColumnTypeError, JSONError, ValueError) as e:
-            error = e.__str__()
-
-        return self.dump_or_error(result, error, callback)
+        return self._safe_get_and_call(dataset_id, _action)
