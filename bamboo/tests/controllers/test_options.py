dee27693590c583642fb6e162f8f8f68ee8f431f
pld <peter@helioid.com>
2012-10-29 10:14:05 -0400

    test all external option routes

--- bamboo/tests/controllers/test_options.py
+++ bamboo/tests/controllers/test_options.py
@@ -1,15 +1,27 @@
 from bamboo.controllers.abstract_controller import AbstractController
+from bamboo.controllers.calculations import Calculations
+from bamboo.controllers.datasets import Datasets
 from bamboo.tests.test_base import TestBase
 
 
 class TestOptions(TestBase):
+    # TODO: check response code == 204
+    # and headers are set correctly...
 
     def setUp(self):
         TestBase.setUp(self)
         self.controller = AbstractController()
 
-    def test_options(self):
-        response = self.controller.options()
+    def _test_options(self, response):
         self.assertTrue(response == '')
-        # TODO: check response code == 204
-        # and headers are set correctly...
+
+    def test_abstract_controller_options(self):
+        response = self.controller.options()
+
+    def test_dataset_controller_options(self):
+        controller = Datasets()
+        response = controller.options('dataset_id')
+
+    def test_calculations_controller_options(self):
+        controller = Calculations()
+        response = self.controller.options('dataset_id')
