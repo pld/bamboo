545a2ae9e1d5015f395df54e98ec1096acece790
modilabs-starscream <modilabs.starscream@gmail.com>
2012-11-09 17:51:42 +0000

    set read and write batch limits

--- bamboo/config/settings.py
+++ bamboo/config/settings.py
@@ -4,7 +4,8 @@ import sys
 # database config
 DATABASE_NAME = 'bamboo_dev'
 TEST_DATABASE_NAME = DATABASE_NAME + '_test'
-DB_BATCH_SIZE = 1000
+DB_SAVE_BATCH_SIZE = 3000
+DB_READ_BATCH_SIZE = 1000
 
 # test settings
 TESTING = 'test' in sys.argv[0].split('/')[-1]
