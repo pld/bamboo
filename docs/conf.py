e3d2ff52667572c7391c750c9dd8685b666885f5
pld <peter@helioid.com>
2012-11-09 17:27:43 -0500

    add inheritance diagram docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ from bamboo.controllers.version import Version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
               'sphinx.ext.pngmath', 'sphinx.ext.mathjax',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+              'sphinx.ext.inheritance_diagram', 'sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
