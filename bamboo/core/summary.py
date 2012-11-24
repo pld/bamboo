509650a4845ea60fe49bc20a85b17a44535d056d
pld <peter@helioid.com>
2012-11-24 00:50:51 -0500

    closes #275, decode summary keys using cleaned-up b64 encode and decode

--- bamboo/core/summary.py
+++ bamboo/core/summary.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from bamboo.lib.jsontools import series_to_jsondict
 from bamboo.lib.schema_builder import DIMENSION, OLAP_TYPE
-from bamboo.lib.mongo import dict_for_mongo
+from bamboo.lib.mongo import dict_from_mongo, dict_for_mongo
 
 
 MAX_CARDINALITY_FOR_COUNT = 10000
@@ -90,7 +90,7 @@ def summarize(dataset, dframe, groups, group_str, no_cache):
         stats.update({group_str: group_stats})
         if not no_cache:
             dataset.update({dataset.STATS: dict_for_mongo(stats)})
-    stats_to_return = stats.get(group_str)
+    stats_to_return = dict_from_mongo(stats.get(group_str))
 
     return stats_to_return if group_str == dataset.ALL else {
         group_str: stats_to_return}
