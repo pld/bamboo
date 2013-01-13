ae5ae81851729a56807ab28571b5682e4d2dc62f
pld <peter@helioid.com>
2013-01-12 19:41:19 -0500

    use quiet install, fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,3 @@
-mkdir -p env
-virtualenv --no-site-packages env
-source env/bin/activate
-echo -e "\nvirtual environment successfully created\n"
 pip install numpy
 pip install -r requirements.pip
 echo -e "requirements successfully installed\n"
