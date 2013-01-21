d4d4dd60f5ee7df908ccbad8ec03fbd94a028050
pld <peter@helioid.com>
2013-01-21 09:41:17 -0500

    bump minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-data',
-    version='0.5.4',
+    version='0.5.4.1',
     author='Modi Research Group',
     author_email='info@modilabs.org',
     packages=['bamboo',
