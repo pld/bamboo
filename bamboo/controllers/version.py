dc25e1f7880c4b7c15c2df9c7db9a75a36c3adda
pld <peter@helioid.com>
2013-01-21 08:49:11 -0500

    bump version to 0.5.4

--- bamboo/controllers/version.py
+++ bamboo/controllers/version.py
@@ -6,7 +6,7 @@ from bamboo.controllers.abstract_controller import AbstractController
 class Version(AbstractController):
 
     # versioning
-    VERSION_NUMBER = '0.5.3'
+    VERSION_NUMBER = '0.5.4'
     VERSION_DESCRIPTION = 'alpha'
 
     def index(self):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bamboo-data',
-    version='0.5.3',
+    version='0.5.4',
     author='Modi Research Group',
     author_email='info@modilabs.org',
     packages=['bamboo',
