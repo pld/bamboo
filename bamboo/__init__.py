d3f12cf492889d17480a08126529dc5951763794
pld <peter@helioid.com>
2013-02-03 11:35:42 -0500

    set celery module consistenly using package init

--- bamboo/__init__.py
+++ bamboo/__init__.py
@@ -1,3 +1,14 @@
-from bamboo.core.frame import *
+import os
+import sys
 
+from bamboo.core.frame import *
 from bamboo.lib.async import *
+
+# test settings
+TESTING = 'test' in sys.argv[0].split('/')[-1]
+
+os.environ['CELERY_CONFIG_MODULE'] = 'bamboo.config.celeryconfig'
+
+if TESTING:
+    set_async(False)
+    os.environ['CELERY_CONFIG_MODULE'] += '_test'
--- bamboo/config/settings.py
+++ bamboo/config/settings.py
@@ -1,7 +1,4 @@
-import os
 import sys
-
-from bamboo.lib.async import is_async, set_async
 
 
 # database config
@@ -9,14 +6,6 @@ ASYNC_FLAG = 'BAMBOO_ASYNC_OFF'
 DATABASE_NAME = 'bamboo_dev'
 TEST_DATABASE_NAME = DATABASE_NAME + '_test'
 
-# test settings
-TESTING = 'test' in sys.argv[0].split('/')[-1]
-
-os.environ['CELERY_CONFIG_MODULE'] = 'bamboo.config.celeryconfig'
-
-if TESTING:
-    set_async(False)
-    os.environ['CELERY_CONFIG_MODULE'] += '_test'
 
 if len(sys.argv) > 1 and 'celeryconfig_test' in sys.argv[1]:
     DATABASE_NAME = TEST_DATABASE_NAME
