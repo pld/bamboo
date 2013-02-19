9b8e323f7f7873c95f856e0da6d9d62841ef3194
renaud gaudin <rgaudin@gmail.com>
2013-02-19 11:06:07 +0000

    Added silent fallback on version check if not on git

--- bamboo/version.py
+++ bamboo/version.py
@@ -8,11 +8,19 @@ VERSION_NUMBER = '%.1f.%d' % (VERSION_MAJOR, VERSION_MINOR)
 VERSION_DESCRIPTION = 'alpha'
 
 
+def safe_command_request(args):
+    try:
+        return check_output(args).strip()
+    except:
+        # might fail at least if git is not present
+        # or if there's no git repository
+        return ''
+
 def get_version():
     return {'version': VERSION_NUMBER,
             'version_major': VERSION_MAJOR,
             'version_minor': VERSION_MINOR,
             'description': VERSION_DESCRIPTION,
-            'branch': check_output([
-                'git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip(),
-            'commit': check_output(['git', 'rev-parse', 'HEAD']).strip()}
+            'branch': safe_command_request([
+                'git', 'rev-parse', '--abbrev-ref', 'HEAD']),
+            'commit': safe_command_request(['git', 'rev-parse', 'HEAD'])}
