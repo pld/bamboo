b9f0552357cb5ae71a8ca704109355be5ada9fc7
pld <peter@helioid.com>
2013-03-03 16:39:41 -0500

    bump version

--- bamboo/lib/version.py
+++ bamboo/lib/version.py
@@ -2,7 +2,7 @@ from subprocess import check_output
 
 # versioning
 VERSION_MAJOR = 0.5
-VERSION_MINOR = 6
+VERSION_MINOR = 7
 VERSION_NUMBER = '%.1f.%d' % (VERSION_MAJOR, VERSION_MINOR)
 VERSION_DESCRIPTION = 'alpha'
 
