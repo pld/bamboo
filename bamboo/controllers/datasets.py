ce4d9df7337cdb0cd19056085a061256d5ccd930
renaud gaudin <rgaudin@gmail.com>
2013-03-07 15:16:19 +0000

    Fix casting on rolling win_type parameter

--- bamboo/controllers/datasets.py
+++ bamboo/controllers/datasets.py
@@ -406,7 +406,7 @@ class Datasets(AbstractController):
         content_type = self.__content_type_for_format(format)
 
         def action(dataset):
-            dframe = dataset.rolling(win_type, int(window))
+            dframe = dataset.rolling(str(win_type), int(window))
             return self.__dataframe_as_content_type(content_type, dframe)
 
         return self._safe_get_and_call(dataset_id, action,
