9ea999b8f6e80b010b1b0247c58ea23f9262f3cf
pld <peter@helioid.com>
2013-05-11 22:23:59 -0400

    rotate dimensional ticks

--- external/bearcart/templates/x_axis_num.js
+++ external/bearcart/templates/x_axis_num.js
@@ -1,6 +1,7 @@
 var x_axis = new Rickshaw.Graph.Axis.X( {
     graph: graph,
     orientation: 'bottom',
+    ticksTreatment: 'glow',
     {{ ticks }}
     element: document.getElementById('x_axis')
 } );
